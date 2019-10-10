af5544c8a690c4f414616231cbf752930b847013
Linesh Raghawan <lra@difi.no>
2019-10-10 14:49:27 +0200

    reninlinze files

---
+++ src/test/java/TestingSPWebTests.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.URL;
+
+public class TestingSPWebTests {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities =  DesiredCapabilities.chrome();
+        capabilities.setCapability("name", "Testing Selenium");
+
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub/"),capabilities);
+    }
+
+    @Test
+    public void testSimple() throws Exception {
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("heelo");
+
+    }
+
+    @AfterClass
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
