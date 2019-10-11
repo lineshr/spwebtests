daccfb989557f80f45f7cfee8ba1f789a824c191
Linesh Raghawan <lra@difi.no>
2019-10-11 13:02:33 +0200

    added aks kluster selenium address

--- src/test/java/TestingSPWebTests.java
+++ src/test/java/TestingSPWebTests.java
@@ -1,5 +1,5 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -9,11 +9,12 @@ import org.testng.annotations.Test;
 import java.net.URL;
 
 public class TestingSPWebTests {
-    private WebDriver driver;
+    private static RemoteWebDriver driver;
 
     @BeforeClass
     public void setUp() throws Exception {
         DesiredCapabilities capabilities =  DesiredCapabilities.chrome();
+        capabilities.setPlatform(Platform.ANY);
         capabilities.setCapability("name", "Testing Selenium");
 
         driver = new RemoteWebDriver(new URL("http://51.144.224.181:4444/wd/hub/"),capabilities);
